ace964a2107ac69c5c148f65f617afd4c5cb5d7c
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-14 13:10:10 +1000

    Update 14-binary_tree_balance.c
    added binary tree height to 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,28 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the tree, or 0 if tree == NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	return (right_height + 1);
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  *
  * @tree: Pointer to the root node of the tree to measure.
