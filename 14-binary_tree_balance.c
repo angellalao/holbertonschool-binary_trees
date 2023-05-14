6bc98b5fc2d4a513d8e089594bb7f9f0b9e3d94d
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-14 13:11:38 +1000

    Update 14-binary_tree_balance.c
    added missing function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: height of the tree, or 0 if tree == NULL
